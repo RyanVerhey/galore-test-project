364175118c8ea489e7eeb97be9ac317b6dcbcd04
RyanVerhey <ryan90v@gmail.com>
2018-12-08 15:53:52 -0500

    Added service class for formatting DateTimes

---
+++ test/services/date_time_formatter_test.rb
@@ -0,0 +1,39 @@
+require 'test_helper'
+
+class DateTimeFormatterTest < ActiveSupport::TestCase
+  class DateTimeFormat < DateTimeFormatterTest
+    test "returns the format of the date" do
+      # Compact
+      assert_equal "%-m/%-d/%Y @ %-l:%M %p %z", DateTimeFormatter.date_time_format(compact: true)
+
+      # Non-compact
+      assert_equal "%A, %B %-d, %Y at %-l:%M %p %z", DateTimeFormatter.date_time_format(compact: false)
+    end
+
+    test "defaults to non-compact" do
+      assert_equal "%A, %B %-d, %Y at %-l:%M %p %z", DateTimeFormatter.date_time_format
+    end
+  end
+
+  class FormatDateTime < DateTimeFormatterTest
+    def setup
+      super
+      @date_time = DateTime.parse "2018/12/8 12:00:00 +0000"
+    end
+
+    test "formats the given DateTime" do
+      # Compact
+      formatted = DateTimeFormatter.format_date_time @date_time, compact: true
+      assert_equal "12/8/2018 @ 12:00 PM +0000", formatted
+
+      # Non-compact
+      formatted = DateTimeFormatter.format_date_time @date_time, compact: false
+      assert_equal "Saturday, December 8, 2018 at 12:00 PM +0000", formatted
+    end
+
+    test "defauts to non-compact" do
+      formatted = DateTimeFormatter.format_date_time @date_time
+      assert_equal "Saturday, December 8, 2018 at 12:00 PM +0000", formatted
+    end
+  end
+end
