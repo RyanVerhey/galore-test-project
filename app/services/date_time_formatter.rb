0efb48ea2ed5902ebfb8bcd2239046f170464932
RyanVerhey <ryan90v@gmail.com>
2018-12-08 16:01:36 -0500

    Changing timezone format

--- app/services/date_time_formatter.rb
+++ app/services/date_time_formatter.rb
@@ -1,6 +1,6 @@
 module DateTimeFormatter
   def self.date_time_format(compact: false)
-    time_format = "%-l:%M %p %Z"
+    time_format = "%-l:%M %p %z"
 
     date_format = compact ? "%-m/%-d/%Y" : "%A, %B %-d, %Y"
     at = compact ? "@" : "at"
