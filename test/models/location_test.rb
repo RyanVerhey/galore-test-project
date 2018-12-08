4624091129148afb75980c28d2c691c926fa00c7
RyanVerhey <ryan90v@gmail.com>
2018-12-08 15:07:59 -0500

    Added #to_s to location

--- test/models/location_test.rb
+++ test/models/location_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class LocationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @location = locations(:one)
+  end
+
+  class ToS < LocationTest
+    def test_returns_the_name_of_the_location
+      assert_equal @location.name, "#{@location}"
+    end
+  end
 end
