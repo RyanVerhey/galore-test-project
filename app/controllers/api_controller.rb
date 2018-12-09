11be1d7c5d16d3a57805fa0a0b1c7bd85f9d8c34
RyanVerhey <ryan90v@gmail.com>
2018-12-09 14:05:39 -0500

    Ensuring all API requests are JSON

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,2 +1,8 @@
 class ApiController < ApplicationController
+  before_action :ensure_json_request
+
+  def ensure_json_request
+    return true if request.format == :json
+    head 406, content_type: 'text/html'
+  end
 end
