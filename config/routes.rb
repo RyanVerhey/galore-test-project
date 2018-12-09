ab12f550fe3be5e402d52038818938d2c8173f95
RyanVerhey <ryan90v@gmail.com>
2018-12-09 15:12:32 -0500

    Don't need any HTML routes for locations

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'events#index'
   resources :events, only: [:index, :show]
-  resources :locations, only: :show
 
   namespace :api, constraints: { format: 'json' } do
     resources :events, only: [:index, :show]
