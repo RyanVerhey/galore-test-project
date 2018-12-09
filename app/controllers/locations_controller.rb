2b222995451ebc8d263ff87c951fbdc4998fe2cc
RyanVerhey <ryan90v@gmail.com>
2018-12-09 14:03:40 -0500

    Removing unneeded comments

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,13 +2,12 @@ class LocationsController < ApplicationController
   before_action :set_location, only: :show
 
   # GET /locations/1
-  # GET /locations/1.json
   def show
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_location
-      @location = Location.find(params[:id])
-    end
+
+  def set_location
+    @location = Location.find(params[:id])
+  end
 end
