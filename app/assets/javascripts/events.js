766f38f0d50d587f3c846a6c6ec49353e573dfe0
RyanVerhey <ryan90v@gmail.com>
2018-12-09 20:28:28 -0500

    Filling in data retrieved from ajax call

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -26,9 +26,20 @@ class EventDetailPopup {
         e.preventDefault();
 
         let eventId = link.getAttribute("data-id");
+        const xhr = new XMLHttpRequest();
+        var self = this;
+
+        xhr.onload = function () {
+          if (xhr.status >= 200 && xhr.status < 300) {
+            self.fillInEventDetails(JSON.parse(xhr.responseText));
+            initMap();
+          }
+        };
+
+        xhr.open('GET', `/ajax/events/${eventId}.json`);
+        xhr.send();
 
         this.popoutDetail.classList.remove("hidden");
-        initMap();
       });
     }
   }
@@ -46,5 +57,27 @@ class EventDetailPopup {
         closePopoutDetail(e);
       }
     });
+  }
+
+  fillInEventDetails(event) {
+    document.getElementById("event-title").innerHTML = event.location.name;
+    document.getElementById("event-title").setAttribute('href', '/events/' + event.id);
+    document.getElementById("location-name").innerHTML = event.location.name;
+    document.getElementById("location-address-1").innerHTML = event.location.address_line_1;
+    if (event.location.address_line_2 !== null) {
+      let address2 = document.getElementById("location-address-2");
+      let lineBreak = document.createElement("br");
+
+      address2.innerHTML = event.location.address_line_2;
+      address2.parentNode.insertBefore(lineBreak, address2.nextSibling);
+    }
+    document.getElementById("location-city").innerHTML = event.location.city;
+    document.getElementById("location-district").innerHTML = event.location.district;
+    document.getElementById("location-post-code").innerHTML = event.location.post_code;
+    document.getElementById("event-description").innerHTML = event.description;
+    document.getElementById("event-start-time").innerHTML = event.start_time;
+    document.getElementById("event-end-time").innerHTML = event.end_time;
+    document.getElementById("latitude").innerHTML = event.location.latitude;
+    document.getElementById("longitude").innerHTML = event.location.longitude;
   }
 }
